0873221c361ffaf40ac79863853b40ebe0aba8b6
Isaac Kwok <kch062522@gmail.com>
2022-04-16 16:20:00 +0800

    feat: adjust new env name to `API_URL`

--- src/hooks/linggle.ts
+++ src/hooks/linggle.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosResponse } from "axios"
 import useSWR from "swr"
-import { NGRAM_API_URL } from "../constants/url"
+import { API_URL } from "../constants/url"
 import LinggleQueryResult from "../types/LinggleQueryResult"
 
 const fetcher = (url: string): Promise<LinggleQueryResult> =>
   axios.get(url).then((res: AxiosResponse): LinggleQueryResult => res.data)
 
 export function useLinggleSearch(query: string | null) {
-  const { data, error } = useSWR(query ? `${NGRAM_API_URL}/${query}` : null, fetcher)
+  const { data, error } = useSWR(query ? `${API_URL}/ngram/${query}` : null, fetcher)
   return {
     results: data,
     error,
